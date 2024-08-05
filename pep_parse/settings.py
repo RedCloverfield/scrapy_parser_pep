fcd46383e96b99881a986315917b8a66561dcf7b
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-05 16:28:26 +0300

    Code refactoring

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,12 +1,14 @@
+from constants import RESULTS_DIR, PEP_SPIDER
+
 BOT_NAME = 'pep_parse'
 
-SPIDER_MODULES = ['pep_parse.spiders']
-NEWSPIDER_MODULE = 'pep_parse.spiders'
+SPIDER_MODULES = [PEP_SPIDER]
+NEWSPIDER_MODULE = PEP_SPIDER
 
 ROBOTSTXT_OBEY = True
 
 FEEDS = {
-    'results/pep_%(time)s.csv': {
+    f'{RESULTS_DIR}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
