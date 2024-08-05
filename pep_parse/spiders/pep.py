143c6b614d3ec186179bb2805dfd1d1983685bc7
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-05 16:29:53 +0300

    Code refactoring

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,28 +1,25 @@
 import scrapy
 
-from constants import PEP_URL
 from ..items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = [PEP_URL, ]
+    start_urls = [f'https://{domain}/' for domain in allowed_domains]
 
     def parse(self, response):
-        links = response.css('#numerical-index table tbody tr a[href^="pep"]')
-        for link in links:
+        for link in response.css(
+            '#numerical-index table tbody tr a[href^="pep"]'
+        ):
             yield response.follow(link, callback=self.parse_pep)
 
     def parse_pep(self, response):
         number, title = ''.join(
             response.css('h1.page-title ::text').getall()
         ).split(' – ')
-        data = {
-            'number': number[4:],
-            'name': title,
-            'status': response.css(
-                'dt:contains("Status") + dd abbr::text'
-            ).get()
-        }
-        yield PepParseItem(data)
+        yield PepParseItem(
+            number=number[4:],
+            name=title,
+            status=response.css('dt:contains("Status") + dd abbr::text').get()
+            )
