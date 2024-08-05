5ae511c5d468c5c5611a446cf077879acf77ced3
Станислав Ефимов <stanislav.karov@mail.ru>
2024-08-05 16:30:25 +0300

    Small changes

--- constants.py
+++ constants.py
@@ -2,5 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 RESULTS_DIR = 'results'
+
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
-PEP_URL = 'https://peps.python.org/'
+
+PEP_SPIDER = 'pep_parse.spiders'
